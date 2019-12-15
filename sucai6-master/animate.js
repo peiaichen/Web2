33a220645ad728cf0f7f3e6c863207e28eaf3399
pyy <652978478.qq.com>
2019-12-15 19:08:26 +0800

    add

---
+++ sucai6-master/animate.js
@@ -0,0 +1,41 @@
+/*
+* @Author: lenovo
+* @Date:   2019-12-15 15:04:21
+* @Last Modified by:   lenovo
+* @Last Modified time: 2019-12-15 15:04:48
+*/
+function getStyle(obj,attr){
+	if(obj.currentStyle){
+		return obj.currentStyle[attr];
+	}else{
+		return getComputedStyle(obj, null)[attr];
+	}
+}
+function animate(obj,json,callback){
+	clearInterval(obj.timer);
+	obj.timer=setInterval(function(){
+		var isStop=true;
+		for(var attr in json){
+			var now=0;
+			if(attr=="opacity"){
+				now=parseInt(getStyle(obj,attr)*100);
+			}else{
+				now=parseInt(getStyle(obj,attr));
+			}
+			var speed=(json[attr]-now)/8;
+			speed=speed>0?Math.ceil(speed):Math.floor(speed);
+			var current=now+speed;
+			if(attr=="opacity"){
+				obj.style.opacity=current/100;
+			}else{
+				obj.style[attr]=current+"px";
+			}
+			if(json[attr]!=current){
+				isStop=false;
+			}
+		}
+		if(isStop){
+			clearInterval(obj.timer);
+		}
+	}, 30)
+}
